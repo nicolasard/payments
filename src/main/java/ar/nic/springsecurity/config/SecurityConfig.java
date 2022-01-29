6eb3e87fdce73b43a21eacda837c3c05a2cc4537
vsinicar <nicolas.ard@gmail.com>
2022-01-29 14:13:12 +0100

    Starting to working with i18n

--- src/main/java/ar/nic/springsecurity/config/SecurityConfig.java
+++ src/main/java/ar/nic/springsecurity/config/SecurityConfig.java
@@ -19,9 +19,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private final UserService userService;
 
-    @Autowired
-    private DataSource dataSource;
-
     public SecurityConfig(UserService userService) {
         this.userService = userService;
     }
