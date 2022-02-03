4149daa14ec58354c9a0c1610d015e0c5ad69fcd
vsinicar <nicolas.ard@gmail.com>
2022-02-04 00:08:25 +0100

    fix

--- getVersion.sh
+++ getVersion.sh
@@ -5,7 +5,8 @@
 echo "Script to get last git version"
 MAJOR=0
 echo "Major version: $MAJOR"
-LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 HEAD)
+#LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 HEAD)
+LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 --tags --always)
 echo "Last version: $LAST_VERSION"
 LAST_MINOR_VERSION=$(echo $LAST_VERSION |sed "s|v${MAJOR}.\([0-9\.]*\).*|\1|")
 NEXT_MINOR_VERSION=$(($LAST_MINOR_VERSION+1))
