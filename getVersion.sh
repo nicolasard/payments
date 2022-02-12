c6800f67089c74b222e3719cf1342881bf92e270
vsinicar <nicolas.ard@gmail.com>
2022-02-12 16:09:23 +0100

    fix

--- getVersion.sh
+++ getVersion.sh
@@ -6,6 +6,7 @@ echo "Script to get last git version"
 MAJOR=0
 echo "Major version: $MAJOR"
 #LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 HEAD)
+# Note: the || : is to make the command always exit 0 in the exit code
 LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 --tags || :)
 if [ -z "${LAST_VERSION}" ]; then
     echo "No minor version tag found for the current major version, creating the first minor..."
@@ -16,5 +17,7 @@ LAST_MINOR_VERSION=$(echo $LAST_VERSION |sed "s|v${MAJOR}.\([0-9\.]*\).*|\1|")
 NEXT_MINOR_VERSION=$(($LAST_MINOR_VERSION+1))
 NEXT_TAG="v${MAJOR}.${NEXT_MINOR_VERSION}"
 echo "Next version: $NEXT_TAG"
-export NEXT_VERSION=$NEXT_TAG
+
+echo "::set-env NEXT_TAG=${NEXT_TAG}"
+
 echo "Use the env. variable NEXT_VERSION to tag and compile the app. LAST_VERSION var is also available"
