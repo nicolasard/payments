26a3d9439cf1455b92a12aa593780584ebd1d82a
vsinicar <nicolas.ard@gmail.com>
2022-02-12 20:13:04 +0100

    fix

--- getVersion.sh
+++ getVersion.sh
@@ -4,11 +4,14 @@
 
 echo "Script to get last git version"
 
+git fetch --unshallow
+git fetch --tags
+
 MAJOR=0
 echo "Major version: $MAJOR"
 #LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 HEAD)
 # Note: the || : is to make the command always exit 0 in the exit code
-LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 --tags --always || :)
+LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 --tags || :)
 if [ -z "${LAST_VERSION}" ]; then
     echo "No minor version tag found for the current major version, creating the first minor..."
     LAST_VERSION=v${MAJOR}.0
