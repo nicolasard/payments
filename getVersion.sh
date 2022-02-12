13759aa51ccd5bb536cb48b5e37e4b10a26c0f88
vsinicar <nicolas.ard@gmail.com>
2022-02-12 12:56:10 +0100

    fix

--- getVersion.sh
+++ getVersion.sh
@@ -6,7 +6,11 @@ echo "Script to get last git version"
 MAJOR=0
 echo "Major version: $MAJOR"
 #LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 HEAD)
-LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 --tags --always)
+LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 --tags)
+if [ -z "${LAST_VERSION}" ]; then
+    echo "No minor version tag found for the current major version, creating the first minor..."
+    LAST_VERSION=0
+fi
 echo "Last version: $LAST_VERSION"
 LAST_MINOR_VERSION=$(echo $LAST_VERSION |sed "s|v${MAJOR}.\([0-9\.]*\).*|\1|")
 NEXT_MINOR_VERSION=$(($LAST_MINOR_VERSION+1))
