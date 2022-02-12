00824ea17d12d30335aa184b22e372313173ce9e
vsinicar <nicolas.ard@gmail.com>
2022-02-12 18:29:52 +0100

    fix

--- tagRepo.sh
+++ tagRepo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Script to tag the repo
 
-echo Taggin next version $NEXT_TAG
+echo Tagging next version $NEXT_TAG
 
-git tag -a $env.NEXT_TAG -m "Automatic tag to $env.NEXT_TAG"
+git config --global user.email "<>"
+git config --global user.name "Bender bot"
+  
+git tag -a $NEXT_TAG -m "Automatic tag to $NEXT_TAG"
