c66cf47eabddff32d708f5f357bdaca1e38e0f7f
Nicolas Ardison <nicolasard@users.noreply.github.com>
2022-02-13 11:12:36 +0100

    Issue 2/create new GitHub action for new version (#4)
    Adding GitHub action to create a new tag.

--- getVersion.sh
+++ getVersion.sh
@@ -3,14 +3,25 @@
 
 
 echo "Script to get last git version"
+
+git fetch --unshallow
+git fetch --tags
+
 MAJOR=0
 echo "Major version: $MAJOR"
 #LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 HEAD)
-LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 --tags --always)
+# Note: the || : is to make the command always exit 0 in the exit code
+LAST_VERSION=$(git describe --match "v$MAJOR.[0-9]*" --abbrev=0 --tags || :)
+if [ -z "${LAST_VERSION}" ]; then
+    echo "No minor version tag found for the current major version, creating the first minor..."
+    LAST_VERSION=v${MAJOR}.0
+fi
 echo "Last version: $LAST_VERSION"
 LAST_MINOR_VERSION=$(echo $LAST_VERSION |sed "s|v${MAJOR}.\([0-9\.]*\).*|\1|")
 NEXT_MINOR_VERSION=$(($LAST_MINOR_VERSION+1))
 NEXT_TAG="v${MAJOR}.${NEXT_MINOR_VERSION}"
 echo "Next version: $NEXT_TAG"
-export NEXT_VERSION=$NEXT_TAG
-echo "Use the env. variable NEXT_VERSION to tag and compile the app. LAST_VERSION var is also available" 
+
+echo "NEXT_TAG=${NEXT_TAG}" >> $GITHUB_ENV
+
+echo "Use the env. env.NEXT_TAG variable to get the last version tag."
