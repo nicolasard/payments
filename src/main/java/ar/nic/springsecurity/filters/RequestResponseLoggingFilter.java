bb87094d19c84b0ea6f26bf8aedad4af8b69b861
vsinicar <nicolas.ard@gmail.com>
2022-06-20 21:43:46 +0200

    #11 Adding uuid

---
+++ src/main/java/ar/nic/springsecurity/filters/RequestResponseLoggingFilter.java
@@ -0,0 +1,37 @@
+package ar.nic.springsecurity.filters;
+
+import ar.nic.springsecurity.services.BillingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@Order(1)
+public class RequestResponseLoggingFilter implements Filter {
+
+    private Logger logger = LoggerFactory.getLogger(RequestResponseLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        final HttpServletRequest req = (HttpServletRequest) request;
+        final HttpServletResponse res = (HttpServletResponse) response;
+        final String uuid = UUID.randomUUID().toString();
+        MDC.put("uuid", uuid);
+        logger.info(
+                "Logging Request  {} : {}", req.getMethod(),
+                req.getRequestURI());
+        chain.doFilter(request, response);
+        logger.info(
+                "Logging Response :{}",
+                res.getContentType());
+    }
+
+}
